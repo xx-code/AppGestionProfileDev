f99acfc3ad0464de483d97f5c2beb8f3b26d0d24
Junior Tiemele <akaaugustejunior@gmail.com>
2023-09-08 21:38:33 -0400

    add test pass check date_start < date_end resume

--- src/transaction_add_resume/mod.rs
+++ src/transaction_add_resume/mod.rs
@@ -1,4 +1,6 @@
 use time::Date;
+use crate::admin_transaction_persistence::AdminTransactionPersistence;
+use crate::admin_transaction_repository::AdminTransactionRepository;
 use crate::resume_transaction_persistence::ResumeTransactionPersistence;
 use crate::resume_transaction_repository::ResumeTransactionRepository;
 use crate::transaction::Transaction;
@@ -7,8 +9,7 @@ use crate::resume::{
     ResumeType
 };
 pub struct TransactionAddResumeCurrent<'a> {
-    db: &'a mut  ResumeTransactionPersistence<'a>,
-    admin_id: &'a String,
+    db_resume: &'a mut  ResumeTransactionPersistence<'a>,
     resume_id: &'a String,
     title: &'a String,
     description: &'a String,
@@ -16,10 +17,10 @@ pub struct TransactionAddResumeCurrent<'a> {
     date_start: &'a Date,
 }
 impl TransactionAddResumeCurrent<'_> {
-    fn new<'a>(db: &'a mut ResumeTransactionPersistence<'a>, admin_id: &'a String, resume_id: &'a String, title: &'a String, description: &'a String, type_resume: &'a ResumeType, date_start: &'a Date) -> TransactionAddResumeCurrent<'a> {
+    fn new<'a>(
+        db_resume: &'a mut ResumeTransactionPersistence<'a>, resume_id: &'a String, title: &'a String, description: &'a String, type_resume: &'a ResumeType, date_start: &'a Date) -> TransactionAddResumeCurrent<'a> {
         TransactionAddResumeCurrent { 
-            db,
-            admin_id, 
+            db_resume,
             resume_id, 
             title, 
             description, 
@@ -30,8 +31,6 @@ impl TransactionAddResumeCurrent<'_> {
 }
 impl Transaction for TransactionAddResumeCurrent<'_> {
     fn execute(&mut self) -> () {
-        let mut do_let_add = false;
-        
         let resume = Resume::new(
             self.resume_id,
             self.title,
@@ -41,19 +40,61 @@ impl Transaction for TransactionAddResumeCurrent<'_> {
             None
         );
 
-        self.db.add_resume(resume);
+        self.db_resume.add_resume(resume);
+    }
+}
+
+pub struct TransactionAddResumeComplet<'a> {
+    db_resume: &'a mut  ResumeTransactionPersistence<'a>,
+    resume_id: &'a String,
+    title: &'a String,
+    description: &'a String,
+    type_resume: &'a ResumeType,
+    date_start: &'a Date,
+    date_end: &'a Date,
+}
+impl TransactionAddResumeComplet<'_> {
+    fn new<'a>(
+        db_resume: &'a mut ResumeTransactionPersistence<'a>, resume_id: &'a String, title: &'a String, description: &'a String, type_resume: &'a ResumeType, date_start: &'a Date, date_end: &'a Date) -> TransactionAddResumeComplet<'a> {
+        TransactionAddResumeComplet { 
+            db_resume,
+            resume_id, 
+            title, 
+            description, 
+            type_resume, 
+            date_start, 
+            date_end
+        }
+    }
+}
+impl Transaction for TransactionAddResumeComplet<'_> {
+    fn execute(&mut self) -> () {
+
+        if self.date_start < self.date_end {
+            let resume = Resume::new(
+                self.resume_id,
+                self.title,
+                self.description,
+                self.type_resume,
+                self.date_start,
+                Some(self.date_end),
+            );
+    
+            self.db_resume.add_resume(resume);
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
-      transaction_create_admin::TransactionCreateAdmin,
       transaction::Transaction,
       resume::ResumeType,
       transaction_add_resume::TransactionAddResumeCurrent, 
       resume_transaction_persistence::ResumeTransactionPersistence, 
-      data_persistence::DataPersistence, resume_transaction_repository::ResumeTransactionRepository,
+      data_persistence::DataPersistence, 
+      resume_transaction_repository::ResumeTransactionRepository,
+      transaction_add_resume::TransactionAddResumeComplet
     };
     use time::Date;
     
@@ -77,7 +118,6 @@ mod tests {
     fn test_add_resume_current() {
         let mut  db = DataPersistence::new();
 
-        let admin_id = String::from("admin1");
         let resume_id = String::from("resume1");
         let title = String::from("title - element");
         let description = String::from("description element");
@@ -88,7 +128,6 @@ mod tests {
 
         let mut ts = TransactionAddResumeCurrent::new(
             &mut resume_data,
-            &admin_id, 
             &resume_id,
             &title, 
             &description, 
@@ -104,29 +143,64 @@ mod tests {
         assert_eq!(resume.get_type_resume(), &type_resume);
         assert_eq!(resume.get_date_start(), &date_start);
     }
-    /*#[test]
-    fn test_verify_if_is_admin_who_add_resume() {
-        
-        let admin_id = String::from("admin1");
+    #[test]
+    fn test_add_resume_complet() {
+        let mut db = DataPersistence::new(); 
+
         let resume_id = String::from("resume1");
         let title = String::from("title - element");
         let description = String::from("description element");
         let type_resume = ResumeType::Education;
         let date_start = Date::from_calendar_date(2021, time::Month::January, 1).unwrap();
+        let date_end = Date::from_calendar_date(2022, time::Month::April, 3).unwrap();
 
-        let ts = TransactionAddResumeCurrent::new(
-            &admin_id, 
+        let mut resume_data = ResumeTransactionPersistence::build(&mut db);
+
+        let mut ts = TransactionAddResumeComplet::new(
+            &mut resume_data,
             &resume_id,
             &title, 
             &description, 
             &type_resume, 
-            &date_start
+            &date_start,
+            &date_end
         );
         ts.execute();
 
-        unsafe {
-            let resume = GLOBAL_DB.get_resume(&resume_id);
-            assert!(resume.is_none());
-        }
-    }*/
+        let resume_data = ResumeTransactionPersistence::build(&mut db);
+        let resume = resume_data.get_resume(&resume_id).unwrap();
+        assert_eq!(resume.get_title(), &title);
+        assert_eq!(resume.get_description(), &description);
+        assert_eq!(resume.get_type_resume(), &type_resume);
+        assert_eq!(resume.get_date_start(), &date_start);
+        assert_eq!(&resume.get_date_end().unwrap(), &date_end);
+    }
+    #[test]
+    fn test_validation_date_resume() {
+        let mut db = DataPersistence::new(); 
+
+        let resume_id = String::from("resume1");
+        let title = String::from("title - element");
+        let description = String::from("description element");
+        let type_resume = ResumeType::Education;
+        let date_start = Date::from_calendar_date(2021, time::Month::January, 1).unwrap();
+        let date_end = Date::from_calendar_date(2020, time::Month::April, 3).unwrap();
+
+        let mut resume_data = ResumeTransactionPersistence::build(&mut db);
+
+        let mut ts = TransactionAddResumeComplet::new(
+            &mut resume_data,
+            &resume_id,
+            &title, 
+            &description, 
+            &type_resume, 
+            &date_start,
+            &date_end
+        );
+        ts.execute();
+
+        let resume_data = ResumeTransactionPersistence::build(&mut db);
+        let resume = resume_data.get_resume(&resume_id);
+        assert!(resume.is_none());
+    }
 }
